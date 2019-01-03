86a79dd5eecc39bf2bd35a6d3f4886a1c123311a
a.kurdyukov <a.kurdyukov@cti.ru>
2019-01-03 21:04:37 +0300

    #add_scrapper ddos protection has been implemented

--- parsers/film_parser.py
+++ parsers/film_parser.py
@@ -5,11 +5,11 @@ class FilmParser:
         self.tree = html.fromstring(page_html)
 
     def get_film_data(self):
-        name = 'DDOS protected sorry'
         movie_name = self.tree.xpath('//div[@id="headerFilm"]//h1[@class="moviename-big"]')
         if movie_name:
             name = movie_name[0].text
-        print('name ', name)
-        return {
-            'name': name
-        }
+            return {
+                'name': name
+            }
+        else:
+            return {}
